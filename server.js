81f9ff78e2a4280cbe07893babf6716bea7ec8d0
yagosenhorini <yagosenhorini30@gmail.com>
2018-09-14 23:58:13 -0300

    Remove dist from swagger

--- server.js
+++ server.js
@@ -3,13 +3,9 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes/productRoutes';
 import cors from 'cors';
-import swagger from 'swagger-node-express';
-import minimist from 'minimist';
 
 const app = express()
 const PORT = 3000
-
-const subpath = express()
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://admin:minu182@mongo-junkyard-gon2z.gcp.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
