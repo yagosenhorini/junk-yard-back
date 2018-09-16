fbd5d9bc088da2794e4bb046ae6a1d93ec559a78
yagosenhorini <yagosenhorini30@gmail.com>
2018-09-16 00:39:10 -0300

    Change cors to manually headers

--- src/bin/server.js
+++ src/bin/server.js
@@ -12,7 +12,20 @@ mongoose.connect('mongodb+srv://admin:minu182@mongo-junkyard-gon2z.gcp.mongodb.n
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json({extended: false}))
-app.use(cors())
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain)
 
 routes(app)
 
